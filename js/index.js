ce9515fbd54182a10148bc62af203c71083c28e1
Rafa P <kadetito@gmail.com>
2019-06-11 21:21:02 +0200

    t4t
    erter

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var app = {
   startCameraAbove: function(){
 	CameraPreview.show();  
-    CameraPreview.startCamera({x: 0, y: 0, width: window.screen.width, height: window.screen.height, camera: "back", toBack: false, previewDrag: true, tapPhoto: true});
+    CameraPreview.startCamera({x: 0, y: 0, width: window.screen.width, height: window.screen.height, camera: "back", toBack: true, previewDrag: true, tapPhoto: true});
   },
 
   startCameraBelow: function(){
